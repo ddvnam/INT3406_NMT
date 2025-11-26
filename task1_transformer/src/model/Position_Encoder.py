5bbe59a2eca2bc1fb522d6ff8a24b6b4c99c5c12
Nguyễn Tiến Phúc <ngtphuc0@gmail.com>
2025-11-26 09:26:26 +0700

    Using pytorch instead of coding scratch

--- task1_transformer/src/model/Position_Encoder.py
+++ task1_transformer/src/model/Position_Encoder.py
@@ -6,40 +6,27 @@ import math
 import logging
 
 class PositionalEncoder(nn.Module):
-    def __init__(self, d_model, max_seq_length=200, dropout=0.1):
+    def __init__(self, d_model, max_len=200, dropout=0.1):
         super().__init__()
-        
-        self.d_model = d_model
         self.dropout = nn.Dropout(dropout)
-        self._max_seq_length = max_seq_length
-        
-        pe = torch.zeros(max_seq_length, d_model)
-        
-        for pos in range(max_seq_length):
-            for i in range(0, d_model, 2):
-                pe[pos, i] = math.sin(pos/(10000**(2*i/d_model)))
-                pe[pos, i+1] = math.cos(pos/(10000**((2*i+1)/d_model)))
-        pe = pe.unsqueeze(0)        
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
-        @torch.jit.script
-        def splice_by_size(source, target):
-            length = target.size(1)
-            return source[:, :length]
+        self.d_model = d_model
 
-        self.splice_by_size = splice_by_size
-    
     def forward(self, x):
-        if(x.shape[1] > self._max_seq_length):
-            logging.warn("Input longer than maximum supported length for PE detected. Build a model with a larger input_max_length limit if you want to keep the input; or ignore if you want the input trimmed")
-            x = x[:, 0:self._max_seq_length]
-        
         x = x * math.sqrt(self.d_model)
-        
-        spliced_pe = self.splice_by_size(self.pe, x) 
-        pe = spliced_pe.requires_grad_(False)
-        
-        x = x + pe
-        x = self.dropout(x)
-        
-        return x
+        seq_len = x.size(1)
+
+        x = x + self.pe[:, :seq_len]
+
+        return self.dropout(x)
+
